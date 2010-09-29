56ef0acd14a89a634800138b63cede17661f1b49
Sean Torrez <smtorrez@umich.edu>
2010-09-29 16:29:26 -0400

    pretty plot updates to work with tex on linux systems. Almost done.

--- pretty_plot.m
+++ pretty_plot.m
@@ -31,7 +31,8 @@ function fh = pretty_plot(fig_handle, varargin)
 
 %% Process the inputs
 h_f = fig_handle;
-figure(h_f);
+% figure(h_f);
+set(h_f, 'visible', 'on');
 h_a = gca;
 
 % Make an options structure.
@@ -54,8 +55,8 @@ opt_names = fieldnames(options);
 n_arg = length(varargin);
 if ~mod(n_arg, 2)
 	% Arguments come in pairs, which is what we want.
-	for i = 1:2:n_arg
-		if ~ischar(varargin{i})
+	for i = 1:n_arg/2
+		if ~ischar(varargin{2*i-1})
 			error('pretty_plot:ArgNotText', ['The first argument of each ' ...
 				'pair should be a property name']);
 		else
@@ -105,10 +106,26 @@ else
 	error('pretty_plot:BadFormat', ...
 		'Format should be text string.')
 end
-		
+
+% The 'Width' field specifies the Width, which was previously specified by
+% Format. Therefore, specifying a Width in addition to a Format means it
+% can be superseded.
+if ischar(options.Width)
+	switch deblank(lower(options.Width))
+		case 'column'
+			Width = 8.2;		% [cm]
+		case 'doublecolumn'
+			Width = 15.2;		% [cm]
+		otherwise
+			error('pretty_plot:UnkWidth', ...
+				['Width ' options.Width ' not recognized.']);
+	end
+else
+	Width = options.Width;
+end
+
 % The aspect ratio is the key parameter to set the height, since in most
 % circumstances the width is limited by some external constraint.
-
 % If a number is given for aspect ratio, use it.
 if any(strcmpi(user_names, 'AspectRatio'))
 	options.AspectRatioMode = 'auto';
@@ -140,6 +157,38 @@ else
 		'Aspect Ratio Mode should be text string.')
 end
 
+% The 'Color' option sets the overall color look of the plot. It's
+% basically like setting a color map. This is optimized for printing in
+% black, because the printer has essentially a different range of blacks
+% from what the screen does.
+if ischar(options.Color)
+	switch deblank(lower(options.Color))
+		case 'black'
+			% Change the color information of the image to be black only
+			TextColor = [0.1 0.1 0.1];
+			map = repmat(linspace(0.75, 1, 64)', [1 3]);
+			colormap(map);
+		case 'color'
+			% Do nothing for now
+			
+		otherwise
+			error('pretty_plot:BadColorValue', ...
+				['Color ' options.Color ' not recognized']);
+	end
+else
+	error('pretty_plot:BadColor', ...
+		'Color should be text string.')
+end
+
+%% Internal options
+
+% Set the tick length. This is always constant, which MATLAB doesn't really
+% want to do, but you can force it.
+TickLength = 0.2;	% [cm]
+% Find the longest dimension
+Longest = max(Height, Width);
+TickLength = TickLength/Longest;
+
 %% First round of plotting.
 % Make the figure the right size.
 fig_pos = get(h_f, 'Position');
@@ -150,34 +199,91 @@ set(h_f, 'Position', [fig_pos(1), fig_pos(2), Width Height])
 % No title for AIAA papers, this is taken care of in LaTex
 
 haxes = get(h_a);
-hxlabel = haxes.XLabel;
-hylabel = haxes.YLabel;
+hlabels = [haxes.XLabel; haxes.YLabel; haxes.ZLabel];
 hlegend = legend(h_a);
 
-set([hxlabel, hylabel]          , ...
-    'FontName'  , 'Times New Roman'       , ...
-    'FontSize'   , 9           , ...
-    'Interpreter', 'latex'      );
-set( hlegend                    , ...
-	'FontName'  , 'Times New Roman'       , ...
-	'FontSize'   , 9           , ...
-	'Interpreter', 'latex'      , ...
-	'Box'       , 'off'         , ...
-	'Location'  , 'Best'        );
+% Get the text and position from each label.
+labels  = get(hlabels);
+strings = {labels.String};
+pos     = reshape([labels.Position],3,3)';
+set(hlabels, 'Units', 'centimeters');
+
+% Determine the system in order to display the fonts properly
+if ispc
+	font_interpreter = 'latex';
+else
+	% Don't use latex because it will mess everything up.
+	font_interpreter = 'latex';
+% 	latexlabels = {};
+	
+	for i = 1:3
+		[unitstr mathstr unittoken] = regexp(strings{1,i}, '([\({\[].*?[\)}\]])', 'match', 'split');
+		if ~isempty(unitstr)
+			texunits{i} = unitstr(1,:);
+		end
+		if ~isempty(mathstr)
+			latexmath{i} = mathstr(1,:);
+		end
+		if ~isempty(unittoken)
+			textokens{i} = unittoken(1,:);
+		end
+		set(hlabels(i), 'String', latexmath{i}{1}, pos);
+		extents(i,:) = get(hlabels, 'Extent');
+		
+		for j = 2:length(latexmath{i}{:})
+			new_pos = [extents(1)+extents(4), extents(2)+extents(5), extents(3)];
+			text(new_pos(1), new_pos(2), new_pos(3), 'hi');
+		end
+		
+	end
+	
+	for i = 1:3
+		[mathstr nonmath mathtoken] = regexp(strings{1,i}, '(\$.*?\$)', 'match', 'split');
+		if ~isempty(mathstr)
+			latexlabels{i} = mathstr(1,:);
+		end
+		if ~isempty(nonmath)
+			texunits{i} = nonmath(1,:);
+		end
+		if ~isempty(mathtoken)
+			textokens{i} = mathtoken(1,:);
+		end
+		set(hlabels(i), 'String', latexlabels{i}{:}, 'Units', 'centimeters');
+	end
+	
+	set(hlabels, 'String', latexlabels, 'Units', 'centimeters');
+	extents = get(hlabels, 'Extent');
+	
+	[mathstr nonmath mathtoken] = regexp(xstring, '(\[.*\])', 'match', 'split');
+	
+end
+
+set([hxlabel, hylabel, hzlabel]             , ...
+	'FontName'   , 'times new roman', ...
+	'FontSize'   , 9                , ...
+	'Interpreter', font_interpreter , ...
+   'Color'      , TextColor       );
+set( hlegend                       , ...
+	'FontName'   , 'times new roman', ...
+	'FontSize'   , 9                , ...
+	'Interpreter', font_interpreter , ...
+	'Box'        , 'off'            , ...
+	'Location'   , 'Best'           , ...
+   'Color'      , TextColor       );
 
 set(h_a, ...
-  'FontName'    , 'Times New Roman'   , ...
-  'FontUnits'   , 'points'  , ...
-  'FontSize'    , 9         , ...
-  'Box'         , 'off'     , ...
-  'TickDir'     , 'out'     , ...
-  'TickLength'  , [.02 .02] , ...
-  'XMinorTick'  , 'on'      , ...
-  'YMinorTick'  , 'on'      , ...
-  'XColor'      , [.1 .1 .1], ...
-  'YColor'      , [.1 .1 .1], ...
-  'YTickLabelMode', 'auto'  , ...
-  'LineWidth'   , 1         );
+	'FontName'      , 'times new roman', ...
+	'FontUnits'     , 'points'         , ...
+	'FontSize'      , 9                , ...
+	'Box'           , 'off'            , ...
+	'TickDir'       , 'out'            , ...
+	'TickLength'    , [TickLength TickLength], ...
+	'XMinorTick'    , 'on'             , ...
+	'YMinorTick'    , 'on'             , ...
+	'XColor'        , TextColor        , ...
+	'YColor'        , TextColor        , ...
+	'YTickLabelMode', 'auto'           , ...
+	'LineWidth'     , 1                );
 
 % Now test to see if the bounds are right
 axes_pos = get(h_a, 'Position');
@@ -191,11 +297,9 @@ t_shift = tight_inset(4);
 set(h_a, 'Position', ...
 	[l_shift, b_shift, Width-l_shift-r_shift, Height-b_shift-t_shift])
 
-% tight_inset = get(h_a, 'TightInset');
-% w_shift = tight_inset(3);
-% h_shift = tight_inset(4);
-
 set(h_f, 'PaperPosition', [0 0 Width Height], ...
 	'PaperSize', [Width Height]);
+
+print(['-f' num2str(fig_handle)], '-dpdf', 'test.pdf');
 
 end
