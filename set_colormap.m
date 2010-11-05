1ca2304dc894b88cc012e3ded5b9fffd1bb7d79c
Derek Dalle <derek.dalle@gmail.com>
2010-11-05 09:21:26 -0400

    Finished coding of set_colormap

--- set_colormap.m
+++ set_colormap.m
@@ -57,9 +57,59 @@ else
 	s_cmap = varargin{i_arg};
 end
 
+% Don't reverse the colormap by default.
+q_cmap_reverse = false;
+
+% Check for a simple string input.
+if ischar(s_cmap)
+	% Check if string begins with 'reverse-'.
+	if numel(s_cmap) > 7 && strcmpi(s_cmap(1:8), 'reverse-')
+		% Drop the first few characters.
+		s_cmap = s_cmap(9:end);
+		% Store parameter to reverse the colormap.
+		q_cmap_reverse = true;
+	end
+	
+	% Try to use a custom colormap.
+	q_cmap_custom = true;
+	% Look for the eight custom colormaps.
+	if strcmpi(s_cmap, 'b') || strcmpi(s_cmap, 'blue')
+		% White - Blue - DarkBlue map
+		s_cmap = {'White', 'Blue', 'DarkBlue'};
+	elseif strcmpi(s_cmap, 'r') || strcmpi(s_cmap, 'red')
+		% White - Red - Black map
+		s_cmap = {'White', 'Red', 'Black'};
+	elseif strcmpi(s_cmap, 'g') || strcmpi(s_cmap, 'green')
+		% White - Green - Black map
+		s_cmap = {'White', 'Lime', 'Black'};
+	elseif strcmpi(s_cmap, 'y') || strcmpi(s_cmap, 'yellow')
+		% White - Yellow - Gold map
+		s_cmap = {'White', 'Yellow', 'Gold'};
+	elseif strcmpi(s_cmap, 'm') || strcmpi(s_cmap, 'magenta')
+		% White - Magenta - DarkMagenta map
+		s_cmap = {'White', 'Magenta', 'DarkMagenta'};
+	elseif strcmpi(s_cmap, 'c') || strcmpi(s_cmap, 'cyan')
+		% White - Cyan - Blue map
+		s_cmap = {'White', 'Cyan', 'Blue'};
+	elseif strcmpi(s_cmap, 'w')
+		% White - LightGrey - Grey map
+		s_cmap = {'White', 'LightGray', 'Gray'};
+	elseif strcmpi(s_cmap, 'k') || strcmpi(s_cmap, 'black')
+		% LightGrey - DarkGray - Black map
+		s_cmap = {'LightGray', 'DarkGray', 'Black'};
+	else
+		% No custom map found.
+		q_cmap_custom = false;
+		% Find the current axes
+		h_a = get(h_f, 'CurrentAxes');
+		% Try to apply the built-in colormap.
+		colormap(h_a, s_cmap);
+	end
+end
 
 
-% Check the type of the variable
+
+% Check the type of the variable.
 if isnumeric(s_cmap)
 	% Number of rows in matrix
 	size_cmap = size(s_cmap);
@@ -241,76 +291,21 @@ elseif iscell(s_cmap)
 	% Apply it.
 	set(h_f, 'ColorMap', v_cmap);
 	
-elseif ischar(s_cmap)
-	% Check if string begins with 'reverse-'.
-	if numel(s_cmap) > 7 && strcmpi(s_cmap(1:8), 'reverse-')
-		% Drop the first few characters.
-		s_cmap = s_cmap(9:end);
-		% Store parameter to reverse the colormap.
-		q_cmap_reverse = true;
-	else
-		% Don't reverse the colormap.
-		q_cmap_reverse = false;
-	end
-	
-	% Try to use a custom colormap.
-	q_cmap_custom = true;
-	% Look for the eight custom colormaps.
-	if strcmpi(s_cmap, 'b') || strcmpi(s_cmap, 'blue')
-		% White - Blue - DarkBlue map
-		v_cmap = [1, 1, 1; 0, 0, 1; 0, 0, 139/255];
-	elseif strcmpi(s_cmap, 'r') || strcmpi(s_cmap, 'red')
-		% White - Red - Black map
-		v_cmap = [1, 1, 1; 1, 0, 0; 0, 0, 0];
-	elseif strcmpi(s_cmap, 'g') || strcmpi(s_cmap, 'green')
-		% White - Green - Black map
-		v_cmap = [1, 1, 1; 0, 1, 0; 0, 0, 0];
-	elseif strcmpi(s_cmap, 'y') || strcmpi(s_cmap, 'yellow')
-		% White - Yellow - Gold map
-		v_cmap = [1, 1, 1; 1, 1, 0; 1, 215/255, 0];
-	elseif strcmpi(s_cmap, 'm') || strcmpi(s_cmap, 'magenta')
-		% White - Magenta - DarkMagenta map
-		v_cmap = [1, 1, 1; 1, 0, 1; 139/255, 0, 139/255];
-	elseif strcmpi(s_cmap, 'c') || strcmpi(s_cmap, 'cyan')
-		% White - Cyan - Blue map
-		v_cmap = [1, 1, 1; 0, 1, 1; 0, 0, 1];
-	elseif strcmpi(s_cmap, 'w')
-		% White - LightGrey - Grey map
-		v_cmap = [255, 255, 255; 211, 211, 211; 128, 128, 128]/255;
-	elseif strcmpi(s_cmap, 'k') || strcmpi(s_cmap, 'black')
-		% LightGrey - DarkGray - Black map
-		v_cmap = [211, 211, 211; 169, 169, 169; 0, 0, 0]/255;
-	else
-		% No custom map found.
-		q_cmap_custom = false;
-	end
-	
-	% Check if a custom colormap was found.
-	if q_cmap_custom
-		% Spacing for initial map.
-		i_cmap = linspace(0, 1, size(v_cmap, 1));
-		% Interpolate into a full map.
-		v_cmap = interp1(i_cmap, v_cmap, linspace(0, 1, 64));
-		% Apply the colormap.
-		set(h_f, 'ColorMap', v_cmap);
-	else
-		% Find the current axes
-		h_a = get(h_f, 'CurrentAxes');
-		% Try to apply the built-in colormap.
-		colormap(h_a, s_cmap);
-	end
-	
-	% Check if the colormap should be reversed.
-	if q_cmap_reverse
-		% Get the current colormap.
-		v_cmap = get(h_f, 'ColorMap');
-		% Apply the reversed version.
-		set(h_f, 'ColorMap', flipud(v_cmap));
-	end
+elseif ischar(s_cmap) && ~q_cmap_custom
+	% Do nothing.
 	
 else
 	% Bad input
 	error('set_colormap:ColorMap', ['ColorMap must be a ', ...
 		'cell array, recognized string, or an Nx3 matrix.']);
 	
+end
+
+	
+% Check if the colormap should be reversed.
+if q_cmap_reverse
+	% Get the current colormap.
+	v_cmap = get(h_f, 'ColorMap');
+	% Apply the reversed version.
+	set(h_f, 'ColorMap', flipud(v_cmap));
 end
