2708e08a6145bc5d1af078dc9855ed193cb10a68
Derek Dalle <derek.dalle@gmail.com>
2012-01-02 14:35:06 -0500

    Added installation scripts

---
+++ src/install.cmd
@@ -0,0 +1,13 @@
+rem
+rem This file automatically runs the installer for set_plot for Windows
+rem operating systems.  It will open a copy of MATLAB within the terminal
+rem provided that a command
+rem
+rem > matlab -nodesktop
+rem
+rem will work on your computer.
+rem
+
+rem Load MATLAB with no desktop and tell it to run the installer and exit.
+matlab -nodesktop -nojvm -nosplash -r "install; exit;"
+
---
+++ src/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#
+# This file automatically runs the installer for set_plot for UNIX-like
+# operating systems.  It will open a copy of MATLAB within the terminal
+# provided that a command
+#
+# $ matlab -nodesktop
+#
+# will work on your computer.
+#
+
+# Load MATLAB with no desktop and tell it to run the installer and exit.
+matlab -nodesktop -nojvm -nosplash -r "install; exit;"
+
