9e207a13e207b5fd9f526d4e22878059c6a71cd6
Derek Dalle <derek.dalle@gmail.com>
2013-10-08 13:48:33 -0400

    Added a license blurb to top of 'sp_Install.m'

--- sp_Install.m
+++ sp_Install.m
@@ -1,3 +1,13 @@
+%-------------------------------------------------------------------------
+% Copyright (c) 2011-2013, Derek J. Dalle <derek.dalle@gmail.com>
+% Copyright (c) 2011, Sean M. Torrez <smtorrez@umich.edu>
+%
+% Distributed under the terms of the Modified BSD Licesne.
+%
+% The full license is available in the file LICENSE, distributed with this
+% software package in the top-level directory.
+%-------------------------------------------------------------------------
+
 function ierr = sp_Install
 %
 % sp_Install
