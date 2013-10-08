a3bc5c83296024ed32617efa6988aec6c45d491b
Derek Dalle <derek.dalle@gmail.com>
2013-10-08 12:15:05 -0400

    Greatly simplified installation script

--- install.sh
+++ install.sh
@@ -11,5 +11,5 @@
 #
 
 # Load MATLAB with no desktop and tell it to run the installer and exit.
-matlab -nodesktop -nojvm -nosplash -r "install; exit;"
+sudo matlab -nodesktop -nojvm -nosplash -r "try, sp_Install;end; exit;"
 
