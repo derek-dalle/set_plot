3cfd6db7528c58a9bd727e2ec94f3bbec3a7d676
Derek Dalle <derek.dalle@gmail.com>
2013-10-09 13:38:01 -0400

    Hopefully working license script

--- script/insert_license.sh
+++ script/insert_license.sh
@@ -14,6 +14,7 @@ files=`find . -name "*.m"`
 
 # File to hold temporary files.
 temp='insertlicensetemp'
+head='insertlicensehead'
 # File containing the .m file license header
 blurb='script/license_m.txt'
 
@@ -23,17 +24,16 @@ do
 	# Say what file is being prepended.
 	echo $file
 	# Write the file from 'function' on to the temp file.
-	cat $file | sed -n '/^function/,$p' > $temp
-	# If the pattern was not found, keep the whole file.
-	if [[ ! -s $temp ]]; then
-		echo "  Pattern not found, prepending entire file."
-		cat $file > $temp
-	fi
-	# Write the file anew, starting with the license.
-	cat $blurb > $file
+	cat $file | sed -n -e '/^function/,/^\s*$/p' > $head
+	cat $file | sed -n '/^% Version/,$p' > $temp
+	# Write the file anew, starting with the header.
+	cat $head > $file
+	# Write he license blurb.
+	cat $blurb >> $file
 	# Write the rest of the file from the tmp.
 	cat $temp > $file
-	# Remove the temp file.
+	# Remove the temp files.
+	rm $head
 	rm $temp
 done
 
